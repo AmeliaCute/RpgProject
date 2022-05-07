a9f25fc27f7a5804d6e2853f174e302fc84dd848
Xelow <XELOXLEGRANDE@gmail.com>
2022-05-07 14:44:33 +0100

    J'ai ajoutez des classes

---
+++ Source/RpgProject/Public/FoodItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryComponent.h"
+#include "FoodItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGPROJECT_API UFoodItem : public UInventoryComponent
+{
+	GENERATED_BODY()
+	
+};
