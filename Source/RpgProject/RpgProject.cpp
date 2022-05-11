f1e7c25b44ca149a83e0838c3497f44c997f42c0
Xelow <XELOXLEGRANDE@gmail.com>
2022-05-11 01:42:46 +0100

    Ntm Unreal :d

--- Source/RpgProject/RpgProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RpgProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RpgProject, "RpgProject" );
