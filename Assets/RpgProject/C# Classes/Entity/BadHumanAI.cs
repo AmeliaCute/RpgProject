39931dfbbf9882eb6ed9685bffc2fbf1aa36ab07
Xelow <XELOXLEGRANDE@gmail.com>
2022-05-30 11:11:20 +0100

    Adding enemies

---
+++ Assets/RpgProject/C# Classes/Entity/BadHumanAI.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+//A simple bad entity ai
+public class BadHumanAI : MonoBehaviour
+{
+    private float distance;
+
+    private float followRange = 6;
+    private float attackRange = 1.7f;
+
+    private float attackCooldown = 1;
+    private float attackTime;
+
+    private float DamageGiven = 16;
+
+    public Transform target;
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        attackTime = Time.time;
+    }
+
+    void Update()
+    {
+        target = GameObject.Find("Player").transform;
+
+        distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance > followRange)
+            Idle();
+        if (distance < followRange && distance > attackRange)
+            Follow();
+        if (distance < attackRange)
+            Attack();
+    }
+
+    void Idle()
+    {
+        return;
+    }
+
+    void Follow()
+    {
+        agent.destination = target.position;
+    }
+
+    void Attack()
+    {
+        if(Time.time > attackTime)
+        {
+            target.GetComponent<Player>().takeDamage(DamageGiven);
+            attackTime = Time.time + attackCooldown;
+        }
+    }
+}
---
+++ Assets/RpgProject/C# Classes/Player/Player.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController Controller;
+    [SerializeField] private Image EnduranceBar;
+    [SerializeField] private Image HealthBar;
+    [SerializeField] private Animation CharacterAnimation;
+
+    [SerializeField] private float WalkingSpeed = 5f;
+    [SerializeField] private float SprintingSpeed = 7f;
+
+    [SerializeField] private float MaxEndurance = 100f;
+    [SerializeField] private float CurrentEndurance;
+
+    [SerializeField] private float MaxHealth = 100f;
+    [SerializeField] private float CurrentHealth;
+
+    private bool isAlive = true;
+    private bool isBusy = false;
+
+    private bool isSprinting = false;
+    private float TargetAngleSmoothTime = 0.1f;
+    private float TargetAngleSmoothVelocity;
+
+    private void Start()
+    {
+        CurrentEndurance = MaxEndurance;
+        CurrentHealth = MaxHealth;
+
+        EnduranceBar.transform.position = new Vector3(100f, 18, 0);
+        HealthBar.transform.position = new Vector3(100f, 34, 0);
+    }
+    void Update()
+    {
+        // Debug
+        debugFunction();
+
+        // Gravity
+        isFlying();
+
+        // Hud
+        updateHud();
+
+
+        if (!isAlive) { dead(); return; } //TODO Faire une animation sur l'ecran lorsque que le joueur meurt
+        if (isBusy) return; 
+
+
+        // Sprinting
+        updateSprint();
+
+        // Move
+        updateMovement();
+    }
+
+    private void updateSprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+            if (isSprinting != true)
+                isSprinting = true;
+        if (Input.GetButtonUp("Sprint"))
+            if (isSprinting != false)
+                isSprinting = false;
+    }
+
+    public void takeDamage(float damage)
+    {
+        CurrentHealth = CurrentHealth - damage; //TODO Prendre en compte les states des items
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            isAlive = false;
+        }
+    }
+    
+    private void dead()
+    {
+        Debug.Log("Player is dead");
+        //... > line 45
+    }
+
+    private void isFlying()
+    {
+        Vector3 moveVector = Vector3.zero;
+        if (Controller.isGrounded == false)
+            moveVector += Physics.gravity;
+        Controller.Move(moveVector * Time.deltaTime);
+    }
+
+    private void updateMovement()
+    {
+        float AxisHor = Input.GetAxisRaw("Horizontal");
+        float AxisVer = Input.GetAxisRaw("Vertical");
+        Vector3 Direction = new Vector3(AxisHor, 0f, AxisVer).normalized;
+
+        if (Direction.magnitude >= 0.1f)
+        {
+            float speed = WalkingSpeed;
+            if (isSprinting)
+            {
+                if (CurrentEndurance != 0)
+                    speed = SprintingSpeed;
+                if (CurrentEndurance > 0)
+                    CurrentEndurance = CurrentEndurance - 0.5f;
+            }
+            else
+                if (CurrentEndurance < MaxEndurance)
+                CurrentEndurance++;
+
+            float TargetAngle = Mathf.Atan2(-Direction.z, Direction.x) * Mathf.Rad2Deg;
+            float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref TargetAngleSmoothVelocity, TargetAngleSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, Angle, 0f);
+
+            Controller.Move(Direction * speed * Time.deltaTime);
+        }
+        else // Endurance
+            if (CurrentEndurance < MaxEndurance)
+            CurrentEndurance++;
+    }
+
+    private void updateHud()
+    {
+        float HealthPerc = ((CurrentHealth * 100) / MaxHealth) / 100;
+        float EndurancePerc = ((CurrentEndurance * 100) / MaxEndurance) / 100;
+
+        HealthBar.fillAmount = HealthPerc;
+        EnduranceBar.fillAmount = EndurancePerc;
+    }
+
+    private void debugFunction()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(10);
+        }
+    }
+}
