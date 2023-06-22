6c3d15884d475fcf4edff1f0ebebbd9cfda9eddf
MignonPetitXelow <xeloxlegrande@gmail.com>
2023-06-22 15:22:23 +0100

    🐞 fix: Fixing RpgSettingsData

--- Assets/RpgProject/Game/Data/ConfigFiles.cs
+++ Assets/RpgProject/Game/Data/ConfigFiles.cs
@@ -5,7 +5,7 @@ namespace RpgProject.Game.Data
     public class RpgSettingsData
     {
         [JsonProperty("VerbosityLevel")]
-        public int VerbosityLevel { get; set; }
+        public int VerbosityLevel;
 
     }
     
